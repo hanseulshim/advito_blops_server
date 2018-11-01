9caa95332491dbba810c6b1f09200faac7a5e391
Hanseul Shim <hanseulshim@gmail.com>
2018-11-01 16:28:42 -0400

    Add mongoose and setup Hotel schema

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
-const Hapi = require('hapi');
+const hapi = require('hapi');
+const mongoose = require('mongoose');
+const Hotel = require('./models/Hotel');
 
-const server = Hapi.server({
+mongoose.connect('mongodb://hshim:B00stlabs!@ds249233.mlab.com:49233/advito_blops_server');
+mongoose.connection.once('open', () => console.log('connected to database'));
+
+const server = hapi.server({
   host:'localhost',
   port:8000
 });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: (request, h) => {
-      return 'hello world';
-  }
-});
+server.route([
+  {
+    method: 'GET',
+    path: '/',
+    handler: (req, h) => {
+      return '<h1>This is my server</h1>';
+    }
+  },
+  {
+    method: 'GET',
+    path: '/api/v1/hotels',
+    handler: (req, h) => {
+      return Hotel.find();
+    }
+  },
+  {
+    method: 'POST',
+    path: '/api/v1/hotels',
+    handler: (req, h) => {
+      const { hotelId, hotelName, address, city, state, latitude, longitude } = req.payload;
+      const hotel = new Hotel({
+        hotelId,
+        hotelName,
+        address,
+        city,
+        state,
+        latitude,
+        longitude
+      });
+      return hotel.save();
+    }
+  },
+]);
 
 const start = async () => {
   try {
