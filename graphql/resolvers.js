e542d0664cbefd2826b42c6ab9d267e67a531017
Hanseul Shim <hanseulshim@gmail.com>
2018-11-07 12:24:21 -0500

    Add addHotel in graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -5,4 +5,15 @@ module.exports = {
     hotels: () => Hotel.find(),
     hotel: (obj, args) => Hotel.findOne({ hotelId: args.hotelId }),
   },
+  Mutation: {
+    addHotel: (obj, args) => new Hotel({
+      hotelId: args.hotelId,
+      hotelName: args.hotelName,
+      address: args.address,
+      city: args.city,
+      state: args.state,
+      latitude: args.latitude,
+      longitude: args.longitude
+    }).save()
+  }
 };
