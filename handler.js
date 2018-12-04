416c634c126b8fdf022806bdf1669baa96aae4b0
Hanseul Shim <hanseulshim@gmail.com>
2018-12-04 15:16:06 -0500

    Remove extra resolvers const

--- handler.js
+++ handler.js
@@ -2,17 +2,6 @@ const { ApolloServer } = require('apollo-server-lambda');
 const { typeDefs } = require('./graphql/typeDefs');
 const { resolvers } = require('./graphql/resolvers');
 
-const resolvers = {
-  Query: {
-    scott: () => 'boss',
-    john: () => 'lil johnny!',
-    shayan: () => 'call linda asap!',
-    hanseul: () => 'han solo!',
-    activeAlerts: () => activeAlerts,
-    upcomingActions: () => upcomingActions,
-  },
-};
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
