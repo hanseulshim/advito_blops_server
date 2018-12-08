380bfd1657aec1d3d10c5fa6d597c2b1945938ae
Hanseul Shim <hanseulshim@Hanseuls-MacBook-Pro.local>
2018-12-08 12:07:32 -0500

    Add personalList

--- data/dashboardData.js
+++ data/dashboardData.js
@@ -14,3 +14,30 @@ exports.programPerformance = [
 ];
 
 exports.noChangeSince = 'July 30';
+
+exports.personaList = [
+  {
+    title: 'road warrior',
+    value: '$3,350',
+    programShare: 25,
+    color: '#ACD2CF',
+  },
+  {
+    title: 'executive',
+    value: '$3,150',
+    programShare: 40,
+    color: '#90C3C1',
+  },
+  {
+    title: 'deal maker',
+    value: '$2,561',
+    programShare: 15,
+    color: '#81BDB9',
+  },
+  {
+    title: 'on demand',
+    value: '$1,955',
+    programShare: 10,
+    color: '#71B5B1',
+  },
+];
