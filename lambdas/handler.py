2d87e38bef13ef3cca765579801fe554fb71528a
William Cahill <williamcahill@Boosts-iMac-3.local>
2018-12-11 13:54:22 -0500

    Creating user hashing function

--- lambdas/handler.py
+++ lambdas/handler.py
@@ -1,5 +1,32 @@
 import json
+import hashlib
+import secrets
+import base64
 
+
+def create_user(event, context):
+
+    """
+    Creates a user given a username, email and password.
+    A random salt of 16 bytes is concatenated at the end of the password before storing.
+    """
+
+    # Unpacks parameters
+    username = event['username']
+    password = event['password']
+    email = event['email']
+
+    # Salts password
+    password_bytes = password.encode(encoding='UTF-8')
+    salt_bytes = secrets.token_bytes(16)
+    combined_bytes = password_bytes + salt_bytes
+
+    # Converts both combined password and salt to base64-encoded strings
+    combined = base64.b64encode(combined_bytes)
+    salt = base64.b64encode(salt_bytes)
+
+    # Stores user in db
+    
 
 def hello(event, context):
     body = {
