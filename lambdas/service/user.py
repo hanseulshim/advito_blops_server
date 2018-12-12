c3a32c40dd8ab47455f76ff51e8844838c8db737
William Cahill <wcahill@boostlabs.com>
2018-12-12 09:35:05 -0500

    Creating hashing and salting example. Incomplete.

--- lambdas/service/user.py
+++ lambdas/service/user.py
@@ -1,3 +1,8 @@
+import secrets
+import base64
+import hashlib
+
+
 class Users:
 
     def __init__(self, db):
@@ -13,7 +18,7 @@ class Users:
     Represents the class that performs logic around a User.
     """
 
-    def create(user):
+    def create(self, user):
         """
         Adds a user into the database
         When inserting the user, both base64(utf8(password + salt)) and base64(salt) are stored.
@@ -24,15 +29,28 @@ class Users:
         password = user['password']
         email = user['email']
 
-        # Salts password
+        # Gets password as bytes and generates a salt
         password_bytes = password.encode(encoding='UTF-8')
         salt_bytes = secrets.token_bytes(16)
-        combined_bytes = password_bytes + salt_bytes
+
+        # Hashes salted password
+        algo = hashlib.sha256()
+        algo.update(password_bytes)
+        algo.update(salt_bytes)
+        hashed_password_bytes = algo.digest()
 
         # Converts both combined password and salt to base64-encoded strings
-        combined = base64.b64encode(combined_bytes)
-        salt = base64.b64encode(salt_bytes)
+        hashed_password = base64 \
+            .b64encode(hashed_password_bytes) \
+            .decode('UTF-8')
+        salt = base64 \
+            .b64encode(salt_bytes) \
+            .decode('UTF-8')
 
-        # Stores user in db
-        connection_str = os.environ['DB_CONNECTION']
-        print(connection_str)
+        # Returns mocked user
+        return {
+            "username": username,
+            "password": hashed_password,
+            "salt": salt,
+            "email": email
+        }
