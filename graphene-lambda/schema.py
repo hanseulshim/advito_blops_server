18b81e58f0aca0ff390188a19414975d28fcd56e
Trey O <Trey@Boosts-iMac.local>
2018-12-24 16:54:02 -0500

    Added a crude login

--- graphene-lambda/schema.py
+++ graphene-lambda/schema.py
@@ -19,6 +19,57 @@ class LoginResponse(graphene.ObjectType):
     statusCode = graphene.Int()
     body = graphene.Field(ResponseBody)
 
+class CreateAdvitoUser(graphene.Mutation):
+    class Arguments:
+        client_id = graphene.Int()
+        username = graphene.String()
+        pwd = graphene.String()
+        name_last = graphene.String()
+        name_first = graphene.String()
+        email = graphene.String()
+        
+    advitoUser = graphene.Field(lambda: AdvitoUser)
+
+    def mutate(self, info, client_id, username, pwd, name_last, name_first, email):
+        advitoUser = AdvitoUser(
+          client_id=client_id, 
+          username=username, 
+          pwd=pwd, 
+          name_last=name_last, 
+          name_first=name_first, 
+          email=email
+        )
+
+        create_user(client_id, username, pwd, name_last, name_first, email)
+
+        return CreateAdvitoUser(advitoUser=advitoUser)
+
+class AdvitoUser(graphene.ObjectType):
+    client_id = graphene.Int()
+    username = graphene.String()
+    pwd = graphene.String()
+    name_last = graphene.String()
+    name_first = graphene.String()
+    email = graphene.String()
+    
+
+class MyMutations(graphene.ObjectType):
+    create_advito_user = CreateAdvitoUser.Field()
+
+def create_user(client_id, username, pwd, name_last, name_first, email):
+    payload_str = '{"client_id": ' + str(client_id) + ', "username": "' + username + '", "pwd": "' + pwd
+    payload_str += '", "name_last": "' + name_last + '", "name_first": "' + name_first + '", "is_enabled": true' 
+    payload_str += ', "email": "' + email + '", "phone": "123-4567", "profile_picture_path": "/",'
+    payload_str += '"timezone_default": "EST", "language_default": "English"}'
+    encoded_str = payload_str.encode('ascii')
+
+    invoke_response = lambda_client.invoke(
+        FunctionName = 'python-lambdas-dev-user_create',
+        InvocationType = 'RequestResponse',
+        ClientContext = base64.b64encode(encoded_str).decode('utf-8'),
+        Payload=bytes(encoded_str)
+    )
+
 def user_login(username, password):
     payload_str = '{"username": "' + username + '", "pwd": "' + password + '"}'
     encoded_str = payload_str.encode('ascii')
@@ -36,37 +87,26 @@ def user_login(username, password):
     if (response_dict['statusCode'] == 200):
         response_payload = json.loads(response_dict["body"])
         dataset = response_payload['apidataset']
+        
         apidataset = ApiDataSet(dataset['displayName'], dataset['sessionToken'])
         responsebody = ResponseBody(response_payload['success'], response_payload['apicode'], response_payload['apimessage'])
         responsebody.apidataset = apidataset
         loginresponse = LoginResponse(response_dict['statusCode'])
         loginresponse.body = responsebody
-        #return response
-        #return apidataset
-        #return responsebody
+        
         return loginresponse
-        #return dataset['sessionToken']
-        #return loginresponse
     else:
         return "Invalide username/password"
 
 class Query(graphene.ObjectType):
-    #login = graphene.String(username=graphene.String(), password=graphene.String())
-    #login = graphene.types.json.JSONString(username=graphene.String(), password=graphene.String())
-    #login = graphene.Field(ApiDataSet, username=graphene.String(), password=graphene.String())
-    login = graphene.Field(ResponseBody, username=graphene.String(), password=graphene.String())
     login = graphene.Field(LoginResponse, username=graphene.String(), password=graphene.String())
+
+    advitoUser = graphene.Field(AdvitoUser)
 
     def resolve_login(self, info, username, password):
         return user_login(username, password)
 
-schema = graphene.Schema(query=Query)
-
-#ads = ApiDataSet('Hello', 'World')
-#print(ads.displayName, ads.sessionToken)
-#rb = ResponseBody(True, 'a', 'b')
-#rb.apidataset = ads
-#print(rb.success)
+schema = graphene.Schema(query=Query, mutation=MyMutations)
 
 
 
