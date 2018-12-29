c28487efc2a725f846856e4a4664e37d71e25fe1
Trey O <Trey@Boosts-iMac.local>
2018-12-29 14:20:15 -0500

    Modified app.py to work with S3 and added the cors.xml.

--- graphene-lambda/app.py
+++ graphene-lambda/app.py
@@ -10,7 +10,7 @@ app.add_url_rule(
     '/graphql',
     view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
-cors = CORS(app, resources={r"/graphql": {"origins": "http://localhost:3000"}})
+cors = CORS(app, resources={r"/graphql": {"origins": "https://s3.amazonaws.com"}})
 
 if __name__ == '__main__':
     app.run(threaded=True, debug=True)
