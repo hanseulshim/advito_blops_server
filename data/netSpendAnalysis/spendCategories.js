6d5e3148e6d804accc6c973bf5fedf780b8e7f60
John O'Neil <joneil@boostlabs.com>
2019-04-04 08:35:32 -0400

    emily

---
+++ data/netSpendAnalysis/spendCategories.js
@@ -0,0 +1,26 @@
+exports.spendCategories = [
+    {
+        title: 'WiFi',
+        icon: 'rooms_icon.png',
+        amount: '$200',
+        diff: -0.3
+    },
+    {
+        title: 'Meals',
+        icon: 'rooms_icon.png',
+        amount: '$375',
+        diff: -0.1
+    },
+    {
+        title: 'Travel',
+        icon: 'rooms_icon.png',
+        amount: '$950',
+        diff: -0.05
+    }, {
+        title: 'Lodging',
+        icon: 'rooms_icon.png',
+        amount: '$1,495',
+        diff: 0.3
+    },
+
+]
