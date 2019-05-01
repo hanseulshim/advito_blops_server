ef89b2ee80047d611ced954832e7956c159a4cff
Anti_Alias <drewlikesramen@gmail.com>
2019-05-01 15:27:14 -0400

    Removing logging code used when testing

--- graphql/resolvers/clientResolvers.js
+++ graphql/resolvers/clientResolvers.js
@@ -1,16 +1,13 @@
 const { lambdaInvoke } = require('../helper');
 
 exports.clientResolvers = {
-  clientList: async (_, payload, { sessionToken }) => {
-    const result = await lambdaInvoke('python-lambdas-dev-client_get_all', {
+  clientList: (_, payload, { sessionToken }) =>
+    lambdaInvoke('python-lambdas-dev-client_get_all', {
       ...payload,
       sessionToken,
-    });
-    console.log(result);
-    return result;
+    }),
   }
-};
-
+  
 exports.clientResolversMutation = {
   updateClient: (_, payload, { sessionToken }) =>
     lambdaInvoke('python-lambdas-dev-client_update', {
